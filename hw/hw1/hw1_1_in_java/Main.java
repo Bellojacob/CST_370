3b36c2c21d311bab2fbb01c2f1510dd9bf25a8fc
Bellojacob <jacobbello2002@gmail.com>
2025-02-03 19:57:08 -0800

    part 1 almost working, part 2 working

--- hw/hw1/hw1_1_in_java/Main.java
+++ hw/hw1/hw1_1_in_java/Main.java
@@ -17,6 +17,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class Main 
@@ -25,10 +26,11 @@ class Main
     {
         // Develop your program here.
         // The following is just a sample statement.
-        System.out.println("ENTER NUMBERS: ");
+        // System.out.println("ENTER NUMBERS: ");
 
         ArrayList<Integer> groupA = new ArrayList<>();
         ArrayList<Integer> groupB = new ArrayList<>();
+        ArrayList<Integer> groupC = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -45,17 +47,81 @@ class Main
 
 
 
-        System.out.println("Group A\n" + groupA);
-        System.out.println("Group B\n" + groupB);
+        // System.out.println("Group A\n" + groupA);
+        // System.out.println("Group B\n" + groupB);
 
 
         //check if group a and group b have intersecting nums
 
+        // loop through group A
         for (int i = 0; i < groupA.size(); i++){
-            if (groupA.get(i).equals(groupB.get(i))) {
-                // if group a at i equals group b at 
+            // set flag for is unique number
+            boolean isUnique = true;
+            // loop through group b
+            for (int k = 0; k < groupB.size(); k++){
+                // if group a num == group b num, then is unique is false, and break the loop and check next num
+                if (groupA.get(i) == groupB.get(k)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            // if we passed through w/o equaling any nums, then it is unique and lets add this to group C
+            if (isUnique) {
+                groupC.add(groupA.get(i));
             }
         }
+
+        // now we check group A against group B for unique nums, and lets check group B against group A
+
+        // lets loop through group B
+        for (int i = 0; i < groupB.size(); i++) {
+            // set is unique flag to true
+            boolean isUnique = true;
+            // lets loop through group A
+            for (int k = 0; k < groupA.size(); k++){
+                // lets check if group B num == group A num
+                if (groupB.get(i) == groupA.get(k)) {
+                    // if we found a match then it is not a unique num, lets break the loop
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                // if nothing matched, then it is unique, but we need to check if it is already in group C, the unique nums from the first check
+                // lets loop through group C and check
+                for (int j = 0; j < groupC.size(); j++){
+                    if (groupB.get(i) == groupC.get(j)) {
+                        isUnique = false;
+                        break;
+                    }
+                    
+                }
+                // if we pass these checks we can assume it is unique and add it
+                groupC.add(groupB.get(i));
+            }
+        }
+
+        // remove duplicates if any
+        ArrayList<Integer> uniqueList = new ArrayList<>();
+
+        for (Integer i : groupC){
+            if (!uniqueList.contains(i)) {
+                uniqueList.add(i);
+            }
+        }
+
+        groupC = uniqueList;
+
+        Collections.sort(groupC, Collections.reverseOrder());
+
+        System.out.print("Answer:");
+        if (groupC.size() == 0) {
+            System.out.print("NONE");
+        }
+        for (int i : groupC) {
+            System.out.print(i + " ");
+        }
+        
         
 
     }
--- hw/hw1/hw1_2_in_java/Main.java
+++ hw/hw1/hw1_2_in_java/Main.java
@@ -16,13 +16,62 @@
  * Date: MM/DD/YYYY
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
 class Main 
 {
     public static void main(String[] args) 
     {
         // Develop your program here.
-        // The following is just a sample statement.
-        System.out.println("Hello world!");
+        
+        HashMap<Integer, Integer> frequentNumbers = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        for (int i = 0; i < size; i++){
+            int temp = scanner.nextInt();
+            if (frequentNumbers.containsKey(temp)){
+                frequentNumbers.put(temp, frequentNumbers.get(temp) + 1);
+            } else {
+            
+            frequentNumbers.put(temp, 1);
+            }
+        }
+
+        // for (int i : frequentNumbers.keySet()){
+        //     System.out.println(i + " - " + frequentNumbers.get(i) );
+        // }
+
+        // look for greatest value frequency
+        ArrayList<Integer> greatestFreq = new ArrayList<>();
+        
+        // Find the highest frequency
+        int largestFrequency = 0;
+        for (int i : frequentNumbers.keySet()) {
+            if (frequentNumbers.get(i) > largestFrequency) {
+                largestFrequency = frequentNumbers.get(i);
+            }
+        }
+
+        // Find numbers that have the highest frequency
+        for (int i : frequentNumbers.keySet()) {
+            if (frequentNumbers.get(i) == largestFrequency) {
+                greatestFreq.add(i);
+            }
+        }
+
+        Collections.sort(greatestFreq);
+
+        System.out.println("Frequency:" + largestFrequency);
+        System.out.print("Number:");
+        for (Integer i : greatestFreq) {
+            System.out.print(i + " ");
+        }
     }
 }
 
